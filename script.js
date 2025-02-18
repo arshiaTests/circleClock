d210afecc63590de321a975c45321ef701ccd878
arshiaTests <arshia.org79@gmail.com>
2025-02-18 21:46:34 +0330

    do smt with js

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let inputCounter = startBox.querySelector('#input-counter');
 let startCounter = startBox.querySelector('#start-counter');
 let errorElement=document.querySelector('#error-message');
 let timerCircle =document.querySelector('.c100');
+let timerNum = document.querySelector('.c100 > span');
+let loadingMessage = document.querySelector('.message .loading')
+let successMessage = document.querySelector('.message .success')
+
 startCounter.addEventListener('click',function(e){
     let seconds = parseInt(inputCounter.value);//عدد را به اینتیجر تبدیل میکند
     if (isNaN(seconds)){
@@ -12,6 +16,27 @@ startCounter.addEventListener('click',function(e){
     }
     errorElement.classList.remove('active');
     startBox.classList.remove('active')
-    timerCircle.style.display = 'block'
+    timerCircle.style.display = 'block';
+    timerNum.textContent = seconds;
+    loadingMessage.style.display = 'block';
+    successMessage.style.display = 'none';            
+    
+
+
+    let timerId = setInterval(() => {
+        if(seconds <= 1) {
+            clearInterval(timerId);
+            startBox.classList.add('active')
+            timerCircle.style.display = 'none';
+            loadingMessage.style.display = 'none';
+            successMessage.style.display = 'block';            
+            inputCounter.value = ''
+        }
+
+
+        seconds -= 1;
+        timerNum.textContent = seconds;
+    }, 1000);
+
 
 })
