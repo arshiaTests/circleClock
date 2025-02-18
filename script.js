c2c3e51b0bf890f297a9178925f2bb370610a6bf
arshiaTests <arshia.org79@gmail.com>
2025-02-18 23:15:46 +0330

    refactor branch

--- script.js
+++ script.js
@@ -1,48 +1,81 @@
-let startBox = document.querySelector('.start-box');
+let startBox = document.querySelector('.start-box')
 let inputCounter = startBox.querySelector('#input-counter');
 let startCounter = startBox.querySelector('#start-counter');
-let errorElement=document.querySelector('#error-message');
-let timerCircle =document.querySelector('.c100');
+let timerCircle = document.querySelector('.c100');
 let timerNum = document.querySelector('.c100 > span');
-let loadingMessage = document.querySelector('.message .loading')
-let successMessage = document.querySelector('.message .success')
+let lastPercent = 'p100'
+let originalSeconds , seconds , timerId;
 
-startCounter.addEventListener('click',function(e){
-    let seconds = parseInt(inputCounter.value);//عدد را به اینتیجر تبدیل میکند
-    if (isNaN(seconds)){
-        errorElement.textContent="زمان را به درستی وارد کنید "
-        errorElement.classList.add('active')
-        return
-    }
-    errorElement.classList.remove('active');
-    startBox.classList.remove('active')
-    timerCircle.style.display = 'block';
-    timerNum.textContent = seconds;
-    loadingMessage.style.display = 'block';
-    successMessage.style.display = 'none';            
-    
+startCounter.addEventListener('click' , function(e) {
+    seconds = parseInt(inputCounter.value)
 
-    let originalSeconds = seconds;
-    let lastPercent = 'p100'
-    let timerId = setInterval(() => {
-        if(lastPercent) timerCircle.classList.remove(lastPercent)
-        if(seconds <= 0) {
-            clearInterval(timerId);
-            startBox.classList.add('active')
-            timerCircle.style.display = 'none';
-            loadingMessage.style.display = 'none';
-            successMessage.style.display = 'block';            
-            inputCounter.value = ''
-            return;
-        }
+    if(isNaN(seconds)) return toggletErrorMessage({ show : true , message : 'زمان را به درستی وارد کنید' });
 
 
-        seconds -= 1;
-        let percent = Math.abs(Math.floor((( (originalSeconds - seconds) / originalSeconds) * 100) - 100))
-        lastPercent = `p${percent}`;
-        timerCircle.classList.add(`p${percent}`)
-        timerNum.textContent = seconds;
-    }, 1000);
+    toggletErrorMessage({ show : false });
+    toggleStartBox({ show : false })
+    toggleLoadingMessage({ show : true })
+    toggleTimer({ show : true , seconds })
 
 
+    originalSeconds = seconds;
+    timerId = setInterval(startTimer, 1000);
 })
+
+let startTimer  = () => {
+    if(lastPercent) timerCircle.classList.remove(lastPercent)
+
+    if(seconds <= 0) {
+        clearInterval(timerId);
+        toggleStartBox({ show : true })
+        toggleTimer({ show : false })
+        toggleLoadingMessage({ show : false })
+        return;
+    }
+
+    seconds -= 1;
+    timerNum.textContent = seconds;
+
+    let percent = lastPercent = `p${Math.abs(Math.floor((( (originalSeconds - seconds) / originalSeconds) * 100) - 100))}`
+    timerCircle.classList.add(percent)
+}
+
+let toggletErrorMessage = ({ show , message }) => {
+    let errorElement = document.querySelector('#error-message');
+    if(show) {
+        errorElement.textContent = message;
+        errorElement.classList.add('active')
+    } else {
+        errorElement.classList.remove('active');
+    }
+}
+
+let toggleStartBox = ({ show }) => {
+    if(show) {
+        startBox.classList.add('active');
+        inputCounter.value = ''
+    } else {
+        startBox.classList.remove('active')
+    }
+}
+
+let toggleTimer = ({ show , seconds }) => {
+    if(show) {
+        timerCircle.style.display = 'block';
+        timerNum.textContent = seconds;
+    } else {
+        timerCircle.style.display = 'none';
+    }
+}
+
+let toggleLoadingMessage = ({ show }) => {
+    let loadingMessage = document.querySelector('.message .loading')
+    let successMessage = document.querySelector('.message .success')    
+    if(show) {
+        loadingMessage.style.display = 'block';
+        successMessage.style.display = 'none';    
+    } else {
+        loadingMessage.style.display = 'none';
+        successMessage.style.display = 'block';       
+    }
+}
